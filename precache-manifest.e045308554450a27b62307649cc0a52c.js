70b0c65799e3219a88849eeeef6b7078a45dce50
Ryan McAndrews <rmcandre915@gmail.com>
2019-11-23 13:37:45 -0600

    Updates

---
+++ precache-manifest.e045308554450a27b62307649cc0a52c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c90aac9272dfb40ed475e5dc14189d4",
+    "url": "/taya-aleksa/index.html"
+  },
+  {
+    "revision": "9205facdeb0e777a7300",
+    "url": "/taya-aleksa/static/css/main.e6227572.chunk.css"
+  },
+  {
+    "revision": "dd3e3a22d59a4e962671",
+    "url": "/taya-aleksa/static/js/2.8d63f668.chunk.js"
+  },
+  {
+    "revision": "9205facdeb0e777a7300",
+    "url": "/taya-aleksa/static/js/main.32690827.chunk.js"
+  },
+  {
+    "revision": "67e7a11f3d96ea4f0d67",
+    "url": "/taya-aleksa/static/js/runtime-main.20e7b9dd.js"
+  },
+  {
+    "revision": "1a5980627e2647b56454998e14eb9907",
+    "url": "/taya-aleksa/static/media/Logo_Black.1a598062.svg"
+  },
+  {
+    "revision": "efd5896922e96a4fb360942feacbbdf0",
+    "url": "/taya-aleksa/static/media/Logo_BlackText.efd58969.svg"
+  },
+  {
+    "revision": "4bf0231dea044e86822a0bdd5cb9e9f2",
+    "url": "/taya-aleksa/static/media/main.4bf0231d.jpg"
+  }
+]);
