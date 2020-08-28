cabbd44824036903a4ffab5ce0f104a4e0b5c947
Ryan McAndrews <rmcandre915@gmail.com>
2020-08-28 15:57:13 -0500

    Updates

---
+++ precache-manifest.197afdd9f741537250d479d1df964cd4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bda2993748a70021fb7d8b2dd04b0e5",
+    "url": "/taya-aleksa/index.html"
+  },
+  {
+    "revision": "aec1a454f04c5f58db33",
+    "url": "/taya-aleksa/static/css/3.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "cf0988cb7609a73bf3b2",
+    "url": "/taya-aleksa/static/css/4.330933b6.chunk.css"
+  },
+  {
+    "revision": "29c7b4e3233eb38e5e84",
+    "url": "/taya-aleksa/static/css/5.4d0a0fb7.chunk.css"
+  },
+  {
+    "revision": "11eb95d7582bd8880c99",
+    "url": "/taya-aleksa/static/css/6.dc88e223.chunk.css"
+  },
+  {
+    "revision": "2064cf634a3908ca1715",
+    "url": "/taya-aleksa/static/css/7.8cce8b5b.chunk.css"
+  },
+  {
+    "revision": "f2fc5c18a1722c1969ab",
+    "url": "/taya-aleksa/static/css/8.1a250bce.chunk.css"
+  },
+  {
+    "revision": "003fb66f326704c7094a",
+    "url": "/taya-aleksa/static/css/main.01cce341.chunk.css"
+  },
+  {
+    "revision": "9622ba35107eb19e5bcf",
+    "url": "/taya-aleksa/static/js/2.fb758c12.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/taya-aleksa/static/js/2.fb758c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec1a454f04c5f58db33",
+    "url": "/taya-aleksa/static/js/3.3cd3f63a.chunk.js"
+  },
+  {
+    "revision": "cf0988cb7609a73bf3b2",
+    "url": "/taya-aleksa/static/js/4.f4ce225c.chunk.js"
+  },
+  {
+    "revision": "29c7b4e3233eb38e5e84",
+    "url": "/taya-aleksa/static/js/5.2af9154e.chunk.js"
+  },
+  {
+    "revision": "11eb95d7582bd8880c99",
+    "url": "/taya-aleksa/static/js/6.0d69b4bc.chunk.js"
+  },
+  {
+    "revision": "2064cf634a3908ca1715",
+    "url": "/taya-aleksa/static/js/7.e58a3e30.chunk.js"
+  },
+  {
+    "revision": "f2fc5c18a1722c1969ab",
+    "url": "/taya-aleksa/static/js/8.e0455803.chunk.js"
+  },
+  {
+    "revision": "822178fe0637d369dfcd",
+    "url": "/taya-aleksa/static/js/9.495d39a0.chunk.js"
+  },
+  {
+    "revision": "003fb66f326704c7094a",
+    "url": "/taya-aleksa/static/js/main.3cb6f203.chunk.js"
+  },
+  {
+    "revision": "3be0173f8f03beaf143d",
+    "url": "/taya-aleksa/static/js/runtime-main.9d33a0cf.js"
+  },
+  {
+    "revision": "1a5980627e2647b56454998e14eb9907",
+    "url": "/taya-aleksa/static/media/Logo_Black.1a598062.svg"
+  },
+  {
+    "revision": "efd5896922e96a4fb360942feacbbdf0",
+    "url": "/taya-aleksa/static/media/Logo_BlackText.efd58969.svg"
+  },
+  {
+    "revision": "9dd853ddabf7f0c223d2b21a3a23d843",
+    "url": "/taya-aleksa/static/media/icon-ig.9dd853dd.svg"
+  },
+  {
+    "revision": "d634e3a6e0a324f28927502daba2212b",
+    "url": "/taya-aleksa/static/media/icon-pin.d634e3a6.svg"
+  },
+  {
+    "revision": "39ffcc8572691a50972011c084366abf",
+    "url": "/taya-aleksa/static/media/icons-linkedin.39ffcc85.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taya-aleksa/precache-manifest.7bb84f1efa3350c9abfdd4a1e66443f2.js"
+  "/taya-aleksa/precache-manifest.197afdd9f741537250d479d1df964cd4.js"
 );
 
 self.addEventListener('message', (event) => {
